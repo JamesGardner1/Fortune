1e5537003e8f223c4574c2279c1deebcc92109de
JamesGardner1 <jamesacgardner@yahoo.com>
2019-12-18 15:32:31 -0600

    Added Shop Intent

--- app/src/main/java/com/example/fortune/MainActivity.java
+++ app/src/main/java/com/example/fortune/MainActivity.java
@@ -2,27 +2,33 @@ package com.example.fortune;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton card1, card2, card3;
     TextView stageText, levelText, healthText, xpText, goldText, goldValue;
-    ProgressBar healthBar, xpBar;
+    ImageView healthBar, xpBar;
 
     Random r = new Random();
 
     Creature bigCreature = new Creature(10, 25, 30, 25, 25);
+    Creature elite = new Creature(15, 30, 60, 50, 0);
+    Creature smallCreatue = new Creature(5, 15, 15, 15, 0);
     Player player = new Player(100, 0, 100, 0);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +47,12 @@ public class MainActivity extends AppCompatActivity {
         healthBar = findViewById(R.id.health_bar);
         xpBar = findViewById(R.id.xp_bar);
 
-        setPlayerStats();
 
-        card1.setImageResource(R.drawable.bigcreature);
-        card2.setImageResource(R.drawable.bigcreature);
-        card2.setContentDescription(getResources().getString(R.string.big_creature));
-        card3.setImageResource(R.drawable.bigcreature);
-        card3.setContentDescription(getResources().getString(R.string.big_creature));
+        // Initial Stats
+        setPlayerStats();
+        // Pulls in base set of cards
+        baseCards();
+
 
         card1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,51 +87,260 @@ public class MainActivity extends AppCompatActivity {
         player.fortune = 100;
     }
 
-    public void setStatsInterface() {
-        goldValue.setText(player.gold);
-
-    }
-
-
     public void pickFortune1() {
-        bigCreature();
-
-
+        Drawable.ConstantState bigCreatureConstantState = getResources().getDrawable(R.drawable.bigcreature).getConstantState();
+        Drawable.ConstantState goldConstantState = getResources().getDrawable(R.drawable.gold).getConstantState();
+        Drawable.ConstantState eliteConstantState = getResources().getDrawable(R.drawable.elite).getConstantState();
+        Drawable.ConstantState healConstantState = getResources().getDrawable(R.drawable.heal).getConstantState();
+        Drawable.ConstantState mysteryConstantState = getResources().getDrawable(R.drawable.mystery).getConstantState();
+        Drawable.ConstantState shopConstantState = getResources().getDrawable(R.drawable.shop).getConstantState();
+        Drawable.ConstantState smallCreatureConstantState = getResources().getDrawable(R.drawable.smallcreatures).getConstantState();
+        if (card1.getDrawable().getConstantState().equals(bigCreatureConstantState)) {
+            bigCreature();
+        } else if (card1.getDrawable().getConstantState().equals(goldConstantState)) {
+            player.gold = player.gold + 100;
+            setStatsInterface();
+        } else if (card1.getDrawable().getConstantState().equals(eliteConstantState)) {
+            eliteBattle();
+        } else if (card1.getDrawable().getConstantState().equals(healConstantState)) {
+            player.hp = player.hp + 35;
+            checkUpdates();
+            shuffleDraw();
+        } else if (card1.getDrawable().getConstantState().equals(mysteryConstantState)) {
+            mysterySelection();
+        } else if (card1.getDrawable().getConstantState().equals(shopConstantState)) {
+            shop();
+        } else if (card1.getDrawable().getConstantState().equals(smallCreatureConstantState)) {
+            smallCreature();
+        } else {
+            Toast.makeText(MainActivity.this, "Error! Please Restart Application", Toast.LENGTH_LONG);
+        }
     }
 
     public void pickFortune2() {
-        if (card2.getContentDescription() == "@string/big_creature") {
+        Drawable.ConstantState bigCreatureConstantState = getResources().getDrawable(R.drawable.bigcreature).getConstantState();
+        Drawable.ConstantState goldConstantState = getResources().getDrawable(R.drawable.gold).getConstantState();
+        Drawable.ConstantState eliteConstantState = getResources().getDrawable(R.drawable.elite).getConstantState();
+        Drawable.ConstantState healConstantState = getResources().getDrawable(R.drawable.heal).getConstantState();
+        Drawable.ConstantState mysteryConstantState = getResources().getDrawable(R.drawable.mystery).getConstantState();
+        Drawable.ConstantState shopConstantState = getResources().getDrawable(R.drawable.shop).getConstantState();
+        Drawable.ConstantState smallCreatureConstantState = getResources().getDrawable(R.drawable.smallcreatures).getConstantState();
+        if (card2.getDrawable().getConstantState().equals(bigCreatureConstantState)) {
             bigCreature();
+        } else if (card2.getDrawable().getConstantState().equals(goldConstantState)) {
+            player.gold = player.gold + 100;
+            setStatsInterface();
+            shuffleDraw();
+        } else if (card2.getDrawable().getConstantState().equals(eliteConstantState)) {
+            eliteBattle();
+        } else if (card2.getDrawable().getConstantState().equals(healConstantState)) {
+            player.hp = player.hp + 35;
+            checkUpdates();
+            shuffleDraw();
+        } else if (card2.getDrawable().getConstantState().equals(mysteryConstantState)) {
+            mysterySelection();
+        } else if (card2.getDrawable().getConstantState().equals(shopConstantState)) {
+            shop();
+        } else if (card2.getDrawable().getConstantState().equals(smallCreatureConstantState)) {
+            smallCreature();
         } else {
-            Toast.makeText(MainActivity.this, "This isnt working", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Error! Please Restart Application", Toast.LENGTH_LONG);
         }
-
     }
 
     public void pickFortune3() {
-        if (card3.getDrawable().equals(R.drawable.bigcreature)) {
+        Drawable.ConstantState bigCreatureConstantState = getResources().getDrawable(R.drawable.bigcreature).getConstantState();
+        Drawable.ConstantState goldConstantState = getResources().getDrawable(R.drawable.gold).getConstantState();
+        Drawable.ConstantState eliteConstantState = getResources().getDrawable(R.drawable.elite).getConstantState();
+        Drawable.ConstantState healConstantState = getResources().getDrawable(R.drawable.heal).getConstantState();
+        Drawable.ConstantState mysteryConstantState = getResources().getDrawable(R.drawable.mystery).getConstantState();
+        Drawable.ConstantState shopConstantState = getResources().getDrawable(R.drawable.shop).getConstantState();
+        Drawable.ConstantState smallCreatureConstantState = getResources().getDrawable(R.drawable.smallcreatures).getConstantState();
+        if (card3.getDrawable().getConstantState().equals(bigCreatureConstantState)) {
             bigCreature();
+        } else if (card3.getDrawable().getConstantState().equals(goldConstantState)) {
+            player.gold = player.gold + 100;
+            setStatsInterface();
+            Toast.makeText(MainActivity.this, "Gained 100 gold", Toast.LENGTH_SHORT).show();
+            shuffleDraw();
+        } else if (card3.getDrawable().getConstantState().equals(eliteConstantState)) {
+            eliteBattle();
+        } else if (card3.getDrawable().getConstantState().equals(healConstantState)) {
+            player.hp = player.hp + 35;
+            checkUpdates();
+            shuffleDraw();
+        } else if (card3.getDrawable().getConstantState().equals(mysteryConstantState)) {
+            mysterySelection();
+        } else if (card3.getDrawable().getConstantState().equals(shopConstantState)) {
+            shop();
+        } else if (card3.getDrawable().getConstantState().equals(smallCreatureConstantState)) {
+            smallCreature();
         } else {
-            Toast.makeText(MainActivity.this, "This isnt working", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Error! Please Restart Application", Toast.LENGTH_LONG).show();
         }
-
     }
 
     public void bigCreature() {
-        int damage = r.nextInt((bigCreature.maxDamage = bigCreature.minDamage) + 1) + bigCreature.minDamage;
-        if (player.fortune > bigCreature.survive) {
-            player.hp = player.hp - damage;
-            player.xp = player.xp + bigCreature.xp;
-            player.gold = player.gold + bigCreature.gold;
+        int damage = r.nextInt((bigCreature.maxDamage - bigCreature.minDamage) + 1) + bigCreature.minDamage;
+        player.hp = player.hp - damage;
+        player.xp = player.xp + bigCreature.xp;
+        player.gold = player.gold + bigCreature.gold;
 
-            Toast.makeText(MainActivity.this, "Lost " + damage + " health! Gained " + bigCreature.gold + " gold and" + bigCreature.xp + " experience!", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Lost " + damage + " health! Gained " + bigCreature.gold + " gold and" + bigCreature.xp + " experience!", Toast.LENGTH_LONG).show();
+        setStatsInterface();
+        checkUpdates();
+        shuffleDraw();
+
+    }
+
+
+
+    public void eliteBattle() {
+        int damage = r.nextInt((elite.maxDamage - elite.minDamage) + 1) + elite.minDamage;
+        player.hp = player.hp - damage;
+        player.xp = player.xp + elite.xp;
+        player.gold = player.gold + elite.gold;
+
+        Toast.makeText(MainActivity.this, "Lost " + damage + " health! Gained " + elite.gold + " gold and" + elite.xp + " experience!", Toast.LENGTH_LONG).show();
+        setStatsInterface();
+        checkUpdates();
+        shuffleDraw();
+    }
+
+    public void mysterySelection() {
+        int mysteryPick = r.nextInt(5) + 1;
+
+        if (mysteryPick == 1) {
+            Toast.makeText(MainActivity.this, "Big Creature!", Toast.LENGTH_SHORT).show();
+            bigCreature();
+        } else if (mysteryPick == 2) {
+            Toast.makeText(MainActivity.this, "Elite!", Toast.LENGTH_SHORT).show();
+            eliteBattle();
+        } else if (mysteryPick == 3) {
+            Toast.makeText(MainActivity.this, "Gold!", Toast.LENGTH_SHORT).show();
+            player.gold = player.gold + 100;
             setStatsInterface();
-        } else {
-            unlucky();
+            shuffleDraw();
+        } else if (mysteryPick == 4) {
+            Toast.makeText(MainActivity.this, "Heal!", Toast.LENGTH_SHORT).show();
+            player.hp = player.hp + 35;
+            checkUpdates();
+            shuffleDraw();
+        } else if (mysteryPick == 5) {
+            Toast.makeText(MainActivity.this, "Shop!", Toast.LENGTH_SHORT).show();
+            shop();
+        } else  {
+            Toast.makeText(MainActivity.this, "Small Creature!", Toast.LENGTH_SHORT).show();
+            smallCreature();
         }
     }
 
-    public void unlucky() {
+    public void shop() {
+        Intent shop = new Intent(MainActivity.this, ShopActivity.class);
+        startActivity(shop);
+        shuffleDraw();
 
     }
+
+
+    public void smallCreature() {
+        int damage = r.nextInt((elite.maxDamage - elite.minDamage) + 1) + elite.minDamage;
+        player.hp = player.hp - damage;
+        player.xp = player.xp + elite.xp;
+        player.gold = player.gold + elite.gold;
+
+        Toast.makeText(MainActivity.this, "Lost " + damage + " health! Gained " + elite.gold + " gold and" + elite.xp + " experience!", Toast.LENGTH_LONG).show();
+        setStatsInterface();
+        checkUpdates();
+        shuffleDraw();
+    }
+
+    public void setStatsInterface() {
+        goldValue.setText(player.gold + "");
+
+    }
+
+    // Base set of Cards
+
+    public void baseCards() {
+        card1.setImageResource(R.drawable.bigcreature);
+        card2.setImageResource(R.drawable.smallcreatures);
+        card3.setImageResource(R.drawable.gold);
+    }
+
+    // Pull up random set of cards
+
+    public void shuffleDraw() {
+        int set = r.nextInt(3) + 1;
+
+        if (set == 1) {
+            set2();
+        } else if (set == 2) {
+            set3();
+        } else if (set == 3) {
+            set4();
+        } else  {
+            set5();
+        }
+    }
+
+    public void set2() {
+        card1.setImageResource(R.drawable.smallcreatures);
+        card2.setImageResource(R.drawable.mystery);
+        card3.setImageResource(R.drawable.heal);
+    }
+
+    public void set3() {
+        card1.setImageResource(R.drawable.shop);
+        card2.setImageResource(R.drawable.elite);
+        card3.setImageResource(R.drawable.bigcreature);
+    }
+
+    public void set4() {
+        card1.setImageResource(R.drawable.mystery);
+        card2.setImageResource(R.drawable.shop);
+        card3.setImageResource(R.drawable.elite);
+    }
+
+    public void set5() {
+        card1.setImageResource(R.drawable.heal);
+        card2.setImageResource(R.drawable.gold);
+        card3.setImageResource(R.drawable.smallcreatures);
+    }
+
+    public void checkUpdates() {
+        if (player.xp > 60 && player.xp < 1000) {
+            levelText.setText("Level 2");
+            xpBar.setImageResource(R.drawable.xp0);
+        }
+
+        if (player.hp >= 100) {
+            healthBar.setImageResource(R.drawable.fullhealth);
+        } else if (player.hp < 100 && player.hp > 70) {
+            healthBar.setImageResource(R.drawable.health75);
+        } else if (player.hp <= 70 && player.hp > 45) {
+            healthBar.setImageResource(R.drawable.health50);
+        } else if (player.hp <= 44 && player.hp < 0) {
+            healthBar.setImageResource(R.drawable.health25);
+        } else {
+            restartGame();
+        }
+
+        if (player.xp == 0) {
+            xpBar.setImageResource(R.drawable.xp0);
+        } else if (player.xp > 0 && player.xp < 15) {
+            xpBar.setImageResource(R.drawable.xp10);
+        } else if (player.xp >= 15 && player.xp < 30) {
+            xpBar.setImageResource(R.drawable.xp30);
+        } else if (player.xp >= 30 && player.xp < 60) {
+            xpBar.setImageResource(R.drawable.xp40);
+        }
+    }
+
+    public void restartGame() {
+
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/fortune/ShopActivity.java
@@ -0,0 +1,93 @@
+package com.example.fortune;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShopActivity extends AppCompatActivity {
+
+    TextView goldText, potionText, prayerBeadsText, swordAndShieldText, expUpText, goldUpText, shopText;
+    ImageView potion, prayerBeads, swordAndShield, expUpSkill, goldUpSkill;
+    Button buyPotionButton, buyPrayerBeadsButton, buySwordAndShieldButton, buyExpUpButton, buyGoldUpButton, backButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.shop);
+
+        goldText = findViewById(R.id.gold_text);
+        potionText = findViewById(R.id.potion_text);
+        prayerBeadsText = findViewById(R.id.prayerbeads_text);
+        swordAndShieldText = findViewById(R.id.swordandshield_text);
+        expUpText = findViewById(R.id.experienceup_text);
+        goldUpText = findViewById(R.id.goldup_text);
+        shopText = findViewById(R.id.shop_text);
+        potion = findViewById(R.id.potion);
+        prayerBeads = findViewById(R.id.prayerbeads);
+        swordAndShield = findViewById(R.id.swordandshield);
+        expUpSkill = findViewById(R.id.expup);
+        goldUpSkill = findViewById(R.id.goldup);
+        buyPotionButton = findViewById(R.id.buy_potion_button);
+        buyPrayerBeadsButton = findViewById(R.id.buy_prayerbeads_button);
+        buySwordAndShieldButton = findViewById(R.id.buy_swordandshield_button);
+        buyExpUpButton = findViewById(R.id.buy_expup_button);
+        buyGoldUpButton = findViewById(R.id.buy_goldup_button);
+        backButton = findViewById(R.id.back_button);
+
+
+        buyPotionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buyPrayerBeadsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buySwordAndShieldButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buyExpUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buyGoldUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnGame();
+            }
+        });
+
+
+    }
+
+    public void returnGame() {
+        Intent returnGame = new Intent(ShopActivity.this, MainActivity.class);
+        startActivity(returnGame);
+    }
+}
